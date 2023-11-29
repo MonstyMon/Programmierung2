cf9993095dfa926a379c645cea10015718181550
Lutritia Dürr <130398628+MonstyMon@users.noreply.github.com>
2023-11-29 12:30:47 +0100

    Pflichtaufgabe 1

---
+++ Pflichtaufgabe1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // System.out.println(Arrays.toString(SiebDesEratosthenes.deleteMultiple(2, new int[]{0,1,8,7,2})));
+        // SiebDesEratosthenes.eratoSieve(30);
+         System.out.println(Arrays.toString(SiebDesEratosthenes.eratoSieve(0)));
+    }
+}
+
